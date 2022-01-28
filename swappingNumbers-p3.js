1ac57b4a600ac1d4421aad8155fca1d43bfa9662
Anoop Kumar <63010032+mr-clover001@users.noreply.github.com>
2022-01-28 13:09:31 +0530

    swapping Numbers

---
+++ swappingNumbers-p3.js
@@ -0,0 +1,12 @@
+let num1 = prompt("enter first A");
+let num2 = prompt("enter second B");
+
+let num3;
+console.log(" Before swapping, Number A and B", num1, num2);
+
+num3 = num1;
+num1 = num2;
+num2 = num3;
+
+console.log(" After swapping, Number A and B is ", num1, num2);
+
