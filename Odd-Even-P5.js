7446497ead2d04b110c852b6da822e6c4a0729ad
Anoop Kumar <63010032+mr-clover001@users.noreply.github.com>
2022-01-28 13:25:22 +0530

    checking the numbers is odd and even

---
+++ Odd-Even-P5.js
@@ -0,0 +1,10 @@
+const num1 = prompt("Enter the number");
+
+if (num1 % 2 == 1) {
+  alert("number is Odd");
+} else if (num1 % 2 == 0) {
+  alert("number is Even");
+} else {
+  alert("default value");
+}
+
