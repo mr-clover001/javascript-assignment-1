396c46ea6bba6062859eda4cc9bfc5b542b07259
Anoop Kumar <63010032+mr-clover001@users.noreply.github.com>
2022-01-28 12:57:22 +0530

    area of triangle

---
+++ area-triangle-p2.js
@@ -0,0 +1,6 @@
+const height = prompt("enter the height of triangle");
+const base = prompt("enter the base of triangle");
+
+const area = (1 / 2) * (height * base);
+
+console.log("Area of triangle  is ", area);
